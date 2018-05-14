4e3836e8053ef0df46d33ff43e42840caf947be0
Ricardo Arango Giraldo <riarangogi@gmail.com>
2018-05-13 22:06:05 -0500

    I added the cand_sta variable  with candidate_statistics tableç

--- script_prueba.R
+++ script_prueba.R
@@ -7,6 +7,7 @@ cnc<-dbConnect(MySQL(),user='product',host='35.184.1.48',
 ##Se enlistan todas las tablas que hay en la base de datos
 dbGetQuery(cnc,statement = 'SHOW TABLES')
 
+##1
 ##Establecemos pues los criterios con  los cuales se calificara la calidad
 ##del candidato
 jobs<-dbGetQuery(cnc,statement = 'SELECT idJob, idCategory, experienceName,
@@ -17,3 +18,10 @@ head(jobs)
 cand<-dbGetQuery(cnc,statement = 'SELECT idUser, idCategory, position, duration
                  FROM work_experiences')
 head(cand)
+
+##2
+cand_sta<-dbGetQuery(cnc,statement = 'SELECT * FROM candidate_statistics')
+str(cand_sta)
+
+##Solo fecha,es decir: año, mes y día
+cand_sta$timeStamp<-as.Date(cand_sta$timeStamp)
